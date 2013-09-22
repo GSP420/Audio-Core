86a02d1426e129ba69cc4a5fad54e0d6628cfb71
Zachary Gauld <zg0789@gmail.com>
2013-09-22 08:50:01 -0700

    Added project to GitHub

---
+++ VS2010 Project/AudioCore/AudioCore/AudioCore.h
@@ -0,0 +1,70 @@
+// GSP 420 - Game Engine Design
+// Audio Core - Header
+// by:
+// Erick Garcia
+// Zachary Gauld
+// Christopher Cone
+// Jordan Swanson
+// Darion Wallace
+
+#ifndef AUDIOCORE_H
+#define AUDIOCORE_H
+#include "fmod.h"
+#include "assert.h"
+
+class Sound
+{
+private:
+	static bool s_enabled;						//is the sound enabled?
+	static char *s_current_sound;				//current sound to play
+
+	//FMOD system declarations
+	static FMOD_RESULT s_result;
+	static FMOD_SYSTEM *s_fmod_system;
+	static FMOD_SOUND *s_sound;
+	static FMOD_CHANNEL *s_channel;
+
+
+public:
+	static void Initialize ();
+	static void Update ();
+
+	//FMOD system declarations
+	static FMOD_SYSTEM* GetSystem ();
+
+	//Sound control
+	static void SetVolume (float vol);			//sets the volume of the playing sound
+	static void Load (const char *filename);	
+	static void Unload ();
+	static void Play (bool pause);
+
+	//Getters
+	static bool GetSoundState ();				//checks whether the sound is on
+	static char* GetCurrentSound ();
+
+	//Setters
+	static void SetPause (bool pause);			//pause or unpause the sound
+	static void SetSound (bool sound);			//set the sound on or off
+
+	//Toggles
+	static void ToggleSound ();
+	static void TogglePause ();
+
+};
+
+class SoundEffect								
+{
+private:
+	bool b_enabled;
+	char* m_sound_name;
+
+	//FMOD system seclarations
+	FMOD_SOUND* m_sound;
+	FMOD_RESULT m_result;
+
+public:
+	SoundEffect (char* filename);
+	void Play ();
+};
+
+#endif
